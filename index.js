a5eaa1abc625ba1310653079712b8c39bd4f39ab
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-07 16:24:01 +0100

    Search feature

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const displayUsers = async () => {
         if (response.ok) {
             const usersObj = await response.json()
 
-            const table = document.querySelector(".table")
+            const table = document.querySelector("tbody")
             usersObj.forEach(user => {
-                const tRow = document.createElement("tbody")
+                const tRow = document.createElement("tr")
                 tRow.innerHTML = 
-                    `<tr>
+                    `
                     <th scope="row">${user.id}</th>
                     <td>${user.name}</td>
                     <td>${user.username}</td>
                     <td>${user.email}</td>
-                    </tr>`
+                    `
 
                 table.appendChild(tRow)
                 console.log(user)
@@ -26,6 +26,35 @@ const displayUsers = async () => {
         console.error(err)
     }
 }
+
+const searchFeature = (event) => {
+    const searchQuery = event.target.value
+    console.log(searchQuery)
+
+    return fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(usersObj => {
+      console.log(usersObj)
+      const resultsArea = document.querySelector("tbody")
+      resultsArea.innerHTML = ""
+      const filteredResult = usersObj.filter((user) => user.name.toLowerCase().includes(searchQuery.toLowerCase()))
+      console.log(filteredResult)
+      filteredResult.forEach(user => {
+        const tRow = document.createElement("tr")
+                tRow.innerHTML = 
+                    `
+                    <th scope="row">${user.id}</th>
+                    <td>${user.name}</td>
+                    <td>${user.username}</td>
+                    <td>${user.email}</td>
+                    `
+        resultsArea.appendChild(tRow)
+      })
+
+    })
+
+    .catch(err => console.log(err))
+  }
 
 window.onload = () => {
     displayUsers();
